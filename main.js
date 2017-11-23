7195b25df8b4980feff63436b29ad62c39575b0a
Philip Karpiak <philip@eswat.ca>
2017-11-23 14:56:21 -0500

    Maximize window and launch DevTools on startup

--- main.js
+++ main.js
@@ -13,7 +13,8 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow();
+  mainWindow.maximize();
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
@@ -23,7 +24,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
