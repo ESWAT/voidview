6a7ed61f6151c66fa803fe6d14d1c6d84a460d82
Philip Karpiak <philip@eswat.ca>
2017-11-26 14:44:51 -0500

    Use electron-webpack and make necessary changes

---
+++ src/main/index.js
@@ -0,0 +1,75 @@
+'use strict'
+
+import { app, BrowserWindow } from 'electron';
+
+const path = require('path');
+const url = require('path');
+const fs = require('fs');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+let window;
+
+function createWindow () {
+  window = new BrowserWindow({
+    "webPreferences": {
+      "webSecurity": false
+    }
+  });
+
+  window.maximize();
+
+  const url = isDevelopment
+    ? `http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}`
+    : `file://${__dirname}/index.html`
+
+  // Open the DevTools.
+  if (isDevelopment) {
+    window.webContents.openDevTools()
+  }
+
+  window.loadURL(url);
+
+  fs.readFile(path.join(__dirname + 'static/app.css'), function(err, dat) {
+    if (!err) {
+      window.webContents.insertCSS(dat)
+    }
+  });
+
+  window.on('closed', () => {
+    window = null;
+  });
+
+  window.webContents.on('devtools-opened', () => {
+    window.focus()
+    setImmediate(() => {
+      window.focus()
+    })
+  })
+
+  return window;
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', () => {
+  window = createWindow();
+});
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,11 +1,24 @@
 // Bootstrap starting state for Yuffie
 
+require("./index.css");
+
 const FILE_LIMIT = 100;
 
 const fs = require('fs');
 const {dialog} = require('electron').remote
 
 const path = dialog.showOpenDialog({properties: ['openDirectory']});
+
+const frame = `
+  <div class="js-list list"></div>
+  <div class="js-peek peek is-none">
+    <div class="js-peek-image peek-image">
+        <button class="js-close-peek close-peek">×</button>
+    </div>
+  </div>
+`;
+
+document.getElementById('app').innerHTML = frame;
 
 fs.readdir(path[0], function(err, dir) {
   if (dir) {
@@ -20,7 +33,7 @@ fs.readdir(path[0], function(err, dir) {
     for (let file of files) {
       let node = document.createElement('div');
       node.classList.add('js-item', 'item');
-      node.setAttribute('style', `background-image: url('${path + '/' + file}')`)
+      node.setAttribute('style', `background-image: url('file://${path + '/' + file}')`)
       node.dataset.image = file;
 
       const item = document.querySelector('.js-list').appendChild(node);
@@ -37,7 +50,7 @@ function openPeek(image) {
 
   if (peekEl.classList.contains('is-none')) {
     const node = peekEl.querySelector('.js-peek-image');
-    node.setAttribute('style', `background-image: url('${path + '/' + image}')`)
+    node.setAttribute('style', `background-image: url('file://${path + '/' + image}')`)
     peekEl.classList.remove('is-none');
     document.body.classList.add('is-frozen');
 
