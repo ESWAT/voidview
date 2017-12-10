ce8ed034d15e1033f0d9b99759b80a83c7aaea1f
Philip Karpiak <philip@eswat.ca>
2017-12-10 11:07:18 -0500

    Fix main window activation condition

--- src/main/index.js
+++ src/main/index.js
@@ -63,7 +63,7 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
+  if (window === null) {
+    createWindow();
   }
 })
