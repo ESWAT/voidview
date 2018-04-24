e4bdfcd158e59dc54981e9678b1d8cce0a6c9838
Philip Karpiak <philip@eswat.ca>
2018-04-24 19:41:36 -0400

    Upgrade deps, particularly webpack4
    Some extra code needed to fix webpack4 + babel7 support

--- webpack.main.js
+++ webpack.main.js
@@ -1,3 +1,9 @@
+// temp workaround for https://github.com/electron-userland/electron-webpack/issues/125
+const presetEnv = require('@babel/preset-env/lib/index.js')
+delete presetEnv.default
+delete presetEnv.isPluginRequired
+delete presetEnv.transformIncludesAndExcludes
+
 module.exports = {
   devtool: 'none'
 }
