5bc4d42edd5faef32ebe703740f09ad85545d7dd
Philip Karpiak <philip@eswat.ca>
2019-07-05 12:07:10 -0400

    Remove unused scripts and refs

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
-const pkg = require('./package.json')
-const {spawn} = require('child_process')
+const { spawn } = require('child_process')
 
 const cmd = process.argv[2]
 const plat = process.argv[3]
@@ -7,9 +6,6 @@ const plat = process.argv[3]
 switch (cmd) {
   case 'dist':
     spawn('electron-builder', ['--x64', 'zip', `--${plat}`], { stdio: 'inherit' })
-    break
-  case 'push':
-    spawn('butler', ['push', `dist/VoidView-${pkg.version}-${plat}.zip`, `eswat/voidview:${plat}`, '--userversion', `${pkg.version}`], { stdio: 'inherit' })
     break
   default:
     console.log('No command given')
