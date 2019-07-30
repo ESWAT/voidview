917ac2f60415d018ddd62ffdbcc57b57dc5a02d6
Philip Karpiak <philip@eswat.ca>
2019-07-30 17:46:09 -0400

    Remove temp webpack/babel7 workarounds

--- webpack.renderer.js
+++ webpack.renderer.js
@@ -1,9 +1,3 @@
-// temp workaround for https://github.com/electron-userland/electron-webpack/issues/125
-const presetEnv = require('@babel/preset-env/lib/index.js')
-delete presetEnv.default
-delete presetEnv.isPluginRequired
-delete presetEnv.transformIncludesAndExcludes
-
 module.exports = {
   devtool: 'none',
   module: {
